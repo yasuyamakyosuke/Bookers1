de4ce85ea582af058964fcbd0eb38795efa7e270
泰山恭輔 <taizankyousuke@taizankyousukenoMacBook-Air.local>
2020-03-16 21:46:15 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def index
+  	@book = Book.new
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  	  @book = Book.new(book_params)
+  	  @book.save
+  	  redirect_to books_path
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
